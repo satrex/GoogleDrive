9fdf1f675b689163d55ec6eb1800d8818d45b9d5
satrex <info@satrex.jp>
2020-08-19 01:42:48 +0900

    Created

---
+++ GoogleDrive/GoogleDriveInternal.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Drive;
+using Google.Apis.Drive.v3;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+using System.Threading;
+
+namespace Satrex.GoogleDrive
+{
+    public class GoogleDriveInternal
+    {
+        static string[] Scopes = { DriveService.Scope.DriveFile };
+        static string ApplicationName = "Google Drive Manipulator";
+
+        private static DriveService service;
+
+        public static DriveService GoogleDriveService
+        {
+            get
+            {
+                if (service == null)
+                {
+                    service = CreateDriveService();
+                }
+                return service;
+            }
+        }
+
+        private static DriveService CreateDriveService()
+        {
+            UserCredential credential;
+            //認証プロセス。credPathが作成されていないとBrowserが起動して認証ページが開くので認証を行って先に進む
+            using (var stream = new FileStream(@"Secrets\client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Path.Combine
+                    (System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                     ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+                //CredentialファイルがcredPathに保存される
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync
+                    (GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None,
+                     new FileDataStore(credPath, true)).Result;
+            }
+            //API serviceを作成、Requestパラメータを設定
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            return service;
+        }
+
+        public static IEnumerable<string> ListFiles(string folderId)
+        {
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = GoogleDriveService.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            return files.Select(file => file.Name);
+        }
+
+        public static void MoveToFolder(string fileId, string folderId)
+        {
+            // Retrieve the existing parents to remove
+            var getRequest = GoogleDriveService.Files.Get(fileId);
+            getRequest.Fields = "parents";
+            var file = getRequest.Execute();
+            var previousParents = String.Join(",", file.Parents);
+            // Move the file to the new folder
+            var updateRequest = GoogleDriveService.Files.Update(new Google.Apis.Drive.v3.Data.File(), fileId);
+            updateRequest.Fields = "id, parents";
+            updateRequest.AddParents = folderId;
+            updateRequest.RemoveParents = previousParents;
+            file = updateRequest.Execute();
+        }
+    }
+}
